5981029754714b9891d847d92a47b92ca9bf538d
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2013-08-25 21:47:13 -0300

    Proyecto rails agregado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mepaseaste::Application.config.secret_key_base = '06d8ef8a6989b852bb0c34cabbfd66efd02e2cb75c973cd209167e787d94456aa7b5d50488ff0258141c280347e87415086879bd562bcad37f62ad4ce3da1497'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mepaseaste::Application.config.session_store :cookie_store, key: '_mepaseaste_session'
