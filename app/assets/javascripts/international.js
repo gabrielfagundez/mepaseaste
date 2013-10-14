fc307f693ca0d8db49c43c68b17327b189268736
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2013-10-14 19:40:36 -0200

    Estilos agregados para soportar la seccion internacional

---
+++ app/assets/javascripts/international.js
@@ -0,0 +1,6 @@
+//= require jquery
+//= require international/main.js
+//= require international/map.js
+//= require international/markers_handling.js
+//= require international/redimension-map.js
+//= require international/send_data_from_map.js
