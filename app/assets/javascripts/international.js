10965f26ea8ddd88e51c01f676898d722b3dce61
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2013-10-29 21:41:18 -0200

    Estilo de botón principal mejorado

--- app/assets/javascripts/international.js
+++ app/assets/javascripts/international.js
@@ -2,6 +2,5 @@
 //= require international/main.js
 //= require shared/map.js
 //= require shared/markers_handling.js
-//= require international/redimension-map.js
 //= require international/send_data_from_map.js
 //= require international/get_cost.js
