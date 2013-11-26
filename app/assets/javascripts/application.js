3a754351ca6905c3e989be12ba1813658803fafa
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2013-11-26 21:51:31 -0200

    Modales agregados al index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,7 @@
-//= require shared/main
+//= require shared/main.js
 //= require shared/map.js
 //= require shared/markers_handling
-//= require application/send_data_from_map.js
-//= require application/get_distance.js
 //= require shared/filling_with_data.js
 //= require shared/routes_handling.js
-
-$(document).foundation();
+//= require application/send_data_from_map.js
+//= require application/get_distance.js
