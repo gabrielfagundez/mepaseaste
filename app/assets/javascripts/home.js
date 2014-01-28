43a54a04d6928cdd2d86ef490755a95cf71fc96c
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2014-01-28 17:50:07 -0200

    Zoom al agregar punto favorito

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,5 +2,10 @@ $(function(){
   $('#agregar_marcador').click(function(){
     createMarkerGivenLatLng($('#agregar_marcador').data().latitude, $('#agregar_marcador').data().longitude);
     $('#modalDestinosUsuales').foundation('reveal', 'close');
+
+    // Zoom al punto ingresado
+    var position = new google.maps.LatLng($('#agregar_marcador').data().latitude, $('#agregar_marcador').data().longitude);
+    map.panTo(position);
+    map.setZoom(15);
   })
 });
