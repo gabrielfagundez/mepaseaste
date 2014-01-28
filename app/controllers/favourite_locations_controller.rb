54b3f590c1419e60692288aca92505a66ccbc7e3
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2014-01-28 17:36:28 -0200

    Controladora de ubicaciones favoritas modificada para nuevas acciones

--- app/controllers/favourite_locations_controller.rb
+++ app/controllers/favourite_locations_controller.rb
@@ -16,4 +16,26 @@ class FavouriteLocationsController < ApplicationController
     end
   end
 
+  def edit
+    @favourite_location = FavouriteLocation.find(params[:id])
+  end
+
+  def update
+    @favourite_location = FavouriteLocation.find(params[:id])
+
+    if @favourite_location.update_attributes(params[:favourite_location])
+      redirect_to queries_path
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    @favourite_location = FavouriteLocation.find(params[:id])
+    @favourite_location.destroy
+
+    redirect_to queries_path
+  end
+  
+
 end
