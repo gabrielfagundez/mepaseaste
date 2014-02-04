374be3a09fe331a1de0ac7559aabb27cc6a2bfe3
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2014-02-04 20:21:34 -0200

    Direcciones arregladas en mostrar resultados

--- app/assets/javascripts/shared/routes_handling.js
+++ app/assets/javascripts/shared/routes_handling.js
@@ -1,43 +1,59 @@
+var iter = 0;
+
 function calcRoute(t) {
 
-    taxi = taxis[t];
-    iter = 0;
+  iter = 0;
 
-    // First, remove any existing markers from the map.
-    for (var i = 0; i < markerArray.length; i++) {
-        markerArray[i].setMap(null);
+  var start, end;
+
+  // Borramos las rutas viejas
+  for(var ds=0; ds<4; ds++){
+    directionsDisplays[ds].setMap(null);
+  }
+
+  // Nos movemos hacia el mapa
+  $('html, body').animate({
+    scrollTop: $("#map").offset().top - 50
+  }, 800);
+
+  // Taxi tiene la forma: [2, 3, 1, 7]
+  taxi = taxis[t];
+
+  // Iteramos en cada tramo y lo dibujamos en el mapa
+  for (it = 0; it < 4; it++){
+
+    // Si el taxi va desde el origen al primer marcador
+    if(it == 0){
+      start = new google.maps.LatLng(getMarkerByID(1).position.lat(), getMarkerByID(1).position.lng());
+      end = new google.maps.LatLng(getMarkerByID(taxi[0]).position.lat(), getMarkerByID(taxi[0]).position.lng());
     }
 
-    // TODO: Eliminar rutas viejas
+    if (it > 0 && it < taxi.length ) {
+      start = new google.maps.LatLng(getMarkerByID(taxi[it-1]).position.lat(), getMarkerByID(taxi[it-1]).position.lng());
+      end = new google.maps.LatLng(getMarkerByID(taxi[it]).position.lat(), getMarkerByID(taxi[it]).position.lng());
+    }
 
-    // Now, clear the array itself.
-    markerArray = [];
+    if(it < taxi.length){
+      // Obtenemos el inicio y el fin y la ruta
+      var route = {
+        origin: start,
+        destination: end,
+        travelMode: google.maps.TravelMode.DRIVING
+      };
 
-    for(var it=0; it<taxi.length; it++){
-        if(it == 0){
-            start = new google.maps.LatLng(getMarkerByID(1).position.lat(), getMarkerByID(1).position.lng());
-            end = new google.maps.LatLng(getMarkerByID(taxi[0]).position.lat(), getMarkerByID(taxi[0]).position.lng());
-        } else {
-            start = new google.maps.LatLng(getMarkerByID(taxi[it-1]).position.lat(), getMarkerByID(taxi[it-1]).position.lng());
-            end = new google.maps.LatLng(getMarkerByID(taxi[it]).position.lat(), getMarkerByID(taxi[it]).position.lng());
+      directionsService.route(route, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+          directionsDisplays[iter].setDirections(response);
+          directionsDisplays[iter].setMap(map);
+
+          iter = iter + 1;
         }
-
-        // Obtenemos el inicio y el fin y la ruta.
-        var request = {
-            origin: start,
-            destination: end,
-            travelMode: google.maps.TravelMode.DRIVING
-        };
-
-        // Dibujamos las rutas y las mostramos en el mapa
-        directionsService.route(request, function(response, status) {
-            if (status == google.maps.DirectionsStatus.OK) {
-                directionsDisplays[iter].setDirections(response);
-                iter = iter + 1;
-                // showSteps(response);
-            }
-        });
+      });
+    } else {
+      directionsDisplays[it].setMap(null);
     }
+
+  }
 }
 
 function centerMap(latitude, longitude){
