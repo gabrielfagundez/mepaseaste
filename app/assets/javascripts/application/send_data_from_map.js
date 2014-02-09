aa420c9a9e171836a5b420990a63f3dc65fec7f3
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2014-02-09 17:06:40 -0200

    Permitir el calculo basado en tarifa nocturna

--- app/assets/javascripts/application/send_data_from_map.js
+++ app/assets/javascripts/application/send_data_from_map.js
@@ -1,42 +1,51 @@
 function sendData() {
 
-    // Mostramos en div de cargando cuando se presiona el botón
-    $('.progress-indicator').show();
+  // Mostramos en div de cargando cuando se presiona el botón
+  $('.progress-indicator').show();
 
-    var obj = new Object();
+  var obj = new Object();
 
-    // Seteamos variables generales
-    obj.tipo_tarifa = 'diurna';
-    obj.marcadores = new Array();
+  // Seteamos el tipo de la tarifa
+  var tarifa;
+  if($('#tarifa_diurna').prop('checked')){
+    tarifa = 'diurna'
+  } else {
+    tarifa = 'nocturna'
+  }
 
-    obj.data_marcadores = new Array();
 
-    // Agregamos la informacion de cada marcador para ser guardada
-    for(var i = 0; i < markers.length; i++){
-        var marcador_temporal = new Object();
+  // Seteamos variables generales
+  obj.tipo_tarifa = tarifa;
+  obj.marcadores = new Array();
 
-        marcador_temporal.location_query_pos    = markers[i].markerId;
-        marcador_temporal.latitude              = markers[i].position.lat();
-        marcador_temporal.longitude             = markers[i].position.lng();
-        marcador_temporal.address               = markers[i].address;
-        marcador_temporal.icon                  = markers[i].icon
+  obj.data_marcadores = new Array();
 
-        obj.data_marcadores.push(marcador_temporal);
-    }
+  // Agregamos la informacion de cada marcador para ser guardada
+  for(var i = 0; i < markers.length; i++){
+    var marcador_temporal = new Object();
 
-    // Agregamos la informacion de distancias
-    for(var i = 0; i < markers.length; i++){
-        obj.marcadores.push(markers[i].distancias_anteriores_raw);
-    }
+    marcador_temporal.location_query_pos    = markers[i].markerId;
+    marcador_temporal.latitude              = markers[i].position.lat();
+    marcador_temporal.longitude             = markers[i].position.lng();
+    marcador_temporal.address               = markers[i].address;
+    marcador_temporal.icon                  = markers[i].icon
 
-    // Convertimos a JSON
-    var json = JSON.stringify(obj);
+    obj.data_marcadores.push(marcador_temporal);
+  }
 
-    $.ajax({
-        contentType: 'application/json',
-        data: json,
-        type: 'POST',
-        url: '/sending_data'
-    });
+  // Agregamos la informacion de distancias
+  for(var i = 0; i < markers.length; i++){
+    obj.marcadores.push(markers[i].distancias_anteriores_raw);
+  }
+
+  // Convertimos a JSON
+  var json = JSON.stringify(obj);
+
+  $.ajax({
+    contentType: 'application/json',
+    data: json,
+    type: 'POST',
+    url: '/sending_data'
+  });
 
 }
