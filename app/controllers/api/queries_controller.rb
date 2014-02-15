913a05052a92552a1fb942d76ac5e09842c55dc9
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2014-02-15 19:42:37 -0200

    Agregada funcion que maneja comunicacion mobile

---
+++ app/controllers/api/queries_controller.rb
@@ -0,0 +1,15 @@
+class QueriesController < ApplicationController
+
+  def create
+    puts params.inspect
+
+    fake_json_response = {
+        tipo_tarifa: 'diurna',
+        costo_total: '20',
+        cantidad_marcadores: 3
+    }
+
+    render json: fake_json_response.to_json
+  end
+
+end
