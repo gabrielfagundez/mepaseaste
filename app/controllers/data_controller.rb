06ac186964c6951fcb687b1ad08503fccad2029f
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2014-04-20 18:51:55 -0300

    Logs agregados para greedy

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -44,8 +44,19 @@ class DataController < ApplicationController
     FileUtils.mkdir_p('public/' + @query.id.to_s)
 
     a_parametros = archivo_de_parametros
+
+    puts '### ### ### ###'
+
+    puts 'Running Greedy'
+    puts "Command: bin/greedy #{ a_parametros } #{ @archivo_costos } > #{ archivo_simplificado_greedy }"
     IO.popen("bin/greedy #{ a_parametros } #{ @archivo_costos } > #{ archivo_simplificado_greedy }")
+    puts 'Greedy end'
+
+    puts 'Running AE'
     IO.popen("bin/genetic_algorithm #{ archivo_de_configuracion } #{ a_parametros } #{ archivo_de_solucion } | grep ^Solution: > #{ archivo_simplificado }")
+    puts 'AE end'
+
+    puts '### ### ### ###'
 
 
     # Renderizamos la nueva pagina
