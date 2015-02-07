68eeddb2d8401fcce5ba16c11a6b623460a40687
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2015-02-07 16:27:28 -0200

    Elimino dos archivos innecesarios

--- app/assets/javascripts/landing.js
+++
@@ -1,9 +0,0 @@
-//= require jquery
-//= require jquery_ujs
-//= require vendor/custom.modernizr
-//= require foundation/foundation
-//= require jquery.onepage-scroll
-
-$(function(){
-  $(".main").onepage_scroll();
-})
