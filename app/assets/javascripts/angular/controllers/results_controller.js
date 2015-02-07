cc956f1641a311071f9ca573abeb1fbd598f72f3
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2015-02-07 19:02:17 -0200

    Agrego marcadores al mapa

--- app/assets/javascripts/angular/controllers/results_controller.js
+++ app/assets/javascripts/angular/controllers/results_controller.js
@@ -12,19 +12,33 @@ app.controller('ResultsController', ['$scope', 'Query', function($scope, Query) 
 
 
   Query.get({ id: 1 }, function(data) {
-    console.log(data)
-
     $scope.totalCost    = '$' + data.costo_total;
     $scope.rateType     = data.tipo_tarifa;
     $scope.markersCount = data.cantidad_marcadores;
     $scope.taxisCount   = data.solution.length;
     $scope.markers      = data.marcadores;
     $scope.taxis        = data.solution;
+
+    $.each(data.marcadores, function (index, marker) {
+      marker = new google.maps.Marker({
+        draggable: false,
+        position: new google.maps.LatLng(marker.latitude, marker.longitude),
+        map: map,
+
+        // App Variables
+        markerId: marker.id,
+        icon: marker.icon,
+        address: marker.address
+      });
+      markers.push(marker);
+    })
   });
 
   $scope.staticMapImageUrl = function (taxi) {
     var width = $('#queries .container').width();
-    var passengers = [];
+    if(width > 750)
+      width = width / 2;
+    var passengers = [$scope.markers[0]];
 
     $.each(taxi, function (index, taxiPassenger) {
       var marker = findMarker(taxiPassenger, $scope.markers);
@@ -33,11 +47,27 @@ app.controller('ResultsController', ['$scope', 'Query', function($scope, Query) 
 
     var latLng = '';
     $.each(passengers, function (index, passenger) {
-      console.log(passenger.latitude, passenger.longitude, index)
       latLng += '|' + passenger.latitude + ',' + passenger.longitude;
     });
-    return 'http://maps.googleapis.com/maps/api/staticmap?size=' + width + 'x300&path=color:0x0000ff|weight:5' + latLng;
+    var url = 'http://maps.googleapis.com/maps/api/staticmap?size=' + width + 'x300&path=color:0x0000ff|weight:5' + latLng;
+    $.each(passengers, function (index, passenger) {
+      if(index == 0)
+        url += '&markers=color:green%7Clabel:O%7C' + passenger.latitude + ',' + passenger.longitude;
+      else
+        url += '&markers=color:blue%7Clabel:D%7C' + passenger.latitude + ',' + passenger.longitude;
+    });
+
+    return url;
   };
+
+  $scope.pasajeros = function(taxi) {
+    var pasajeros = '';
+    $.each(taxi, function (index, pasajero) {
+      pasajeros += pasajero + ', ';
+    })
+
+    return pasajeros;
+  }
 
   function findMarker(id, markers) {
     var marker;
